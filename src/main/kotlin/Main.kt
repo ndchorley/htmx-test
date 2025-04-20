9565601357a30ac9932cded9adafefcaee7b0174
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-20 20:37:11 +0100

    Add an element that's updated every 2 s with htmx and an endpoint to get the latest message for that

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 package org.example
 
+import org.http4k.core.ContentType
 import org.http4k.core.Filter
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
+import org.http4k.lens.contentType
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
@@ -13,11 +15,19 @@ import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
 import org.http4k.template.ViewModel
 import java.time.Clock
+import java.time.LocalDate
 import java.time.ZoneId
+import java.time.format.TextStyle
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedDeque
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit.SECONDS
+import kotlin.random.Random
 
 val renderTemplate = HandlebarsTemplates().CachingClasspath()
 
 object View : ViewModel
+data class Message(val message: String) : ViewModel
 
 val printRequest = Filter { next ->
     { request ->
@@ -31,6 +41,8 @@ val printRequest = Filter { next ->
 }
 
 var countOfClicks = 0
+
+val messages = ConcurrentLinkedDeque<String>()
 
 val router =
     routes(
@@ -46,11 +58,47 @@ val router =
                 if (countOfClicks == 1) "time" else "times"
 
             Response(OK).body("Clicked $countOfClicks $timeOrTimes")
+        },
+
+        "/latest-message" bind GET to { _ ->
+            val latestMessage = messages.removeFirst()
+
+            Response(OK)
+                .contentType(ContentType.TEXT_HTML)
+                .body(renderTemplate(Message(latestMessage)))
         }
     )
 
 fun main() {
+    addMessagesAtRegularIntervals()
+
     printRequest.then(router)
         .asServer(Jetty(8000))
         .start()
+}
+
+fun addMessagesAtRegularIntervals() {
+    val messagesToAdd = listOf(
+        "Hello",
+        "Goodbye",
+        "This is a test",
+        "It's ${LocalDate.now().dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH)}",
+        "This app was written with http4k and htmx",
+        "The quick brown fox jumped over the lazy dog",
+        "Lorem ipsum",
+        "Ich komme aus London",
+        "J'irai en France cette année",
+        "Am Samstagvormittag lerne ich Deutsch",
+        "My very easy method just speeds up naming planets",
+        "José Raúl Capablanca was world champion from 1921 to 1927"
+    )
+
+    val scheduler = Executors.newScheduledThreadPool(1)
+    val random = Random(Clock.systemDefaultZone().millis())
+
+    scheduler.scheduleAtFixedRate({
+        val message = messagesToAdd[random.nextInt(0, messagesToAdd.size)]
+
+        messages.addLast(message)
+    }, 5, 3, SECONDS)
 }
