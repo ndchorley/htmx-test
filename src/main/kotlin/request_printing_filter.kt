44afca89fc7ea642fdb0568512a0881e71fb4839
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-21 10:43:48 +0100

    Refactor to introduce timeNow function in request_printing_filter.kt

--- src/main/kotlin/request_printing_filter.kt
+++ src/main/kotlin/request_printing_filter.kt
@@ -4,14 +4,15 @@ import org.http4k.core.Filter
 import java.time.Clock
 import java.time.ZoneId
 
-private val london = ZoneId.of("Europe/London")
-
 val printRequest = Filter { next ->
     { request ->
-        val timeNow = Clock.systemDefaultZone().instant().atZone(london)
-
-        println("$timeNow ${request.method} ${request.uri}")
+        println("${timeNow()} ${request.method} ${request.uri}")
 
         next(request)
     }
 }
+
+private val london = ZoneId.of("Europe/London")
+
+private fun timeNow() =
+    Clock.systemDefaultZone().instant().atZone(london)
