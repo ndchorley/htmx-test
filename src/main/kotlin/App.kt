818163c290ac41d72547dda4618547ed610afd4a
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-21 10:24:56 +0100

    Move App to its own file

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,20 @@
+package org.example
+
+import org.http4k.core.Method.GET
+import org.http4k.core.then
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+import java.util.concurrent.ConcurrentLinkedDeque
+
+class App(messages: ConcurrentLinkedDeque<String>) {
+    val router =
+        routes(
+            "/" bind GET to ::homePage,
+            "/latest-message" bind GET to LatestMessage(messages)
+        )
+
+    val server = printRequest.then(router)
+        .asServer(Jetty(8000))
+}
