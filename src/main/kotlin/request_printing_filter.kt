313913a49e4cbe7bc702731e3b6f37efcdc73da3
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-04-21 10:29:38 +0100

    Move london val to file scope in request_printing_filter.kt

--- src/main/kotlin/request_printing_filter.kt
+++ src/main/kotlin/request_printing_filter.kt
@@ -4,9 +4,10 @@ import org.http4k.core.Filter
 import java.time.Clock
 import java.time.ZoneId
 
+private val london = ZoneId.of("Europe/London")
+
 val printRequest = Filter { next ->
     { request ->
-        val london = ZoneId.of("Europe/London")
         val timeNow = Clock.systemDefaultZone().instant().atZone(london)
 
         println("$timeNow ${request.method} ${request.uri}")
