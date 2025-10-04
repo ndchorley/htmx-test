e95e736f95fff4b6f4727eb56acde24da4954610
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2025-10-04 17:55:13 +0100

    Don't stop htmx from making requests

--- src/main/kotlin/LatestMessage.kt
+++ src/main/kotlin/LatestMessage.kt
@@ -7,14 +7,7 @@ import java.util.concurrent.ConcurrentLinkedDeque
 class LatestMessage(
     private val messages: ConcurrentLinkedDeque<String>
 ) : HttpHandler {
-    private var requestCount = 0
-
     override fun invoke(request: Request): Response {
-        requestCount++
-
-        if (requestCount > 40)
-            return Response(Status(286, null))
-
         val latestMessage = messages.removeFirst()
 
         val html = renderTemplate(Message(latestMessage))
